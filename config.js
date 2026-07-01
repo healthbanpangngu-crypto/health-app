a6015037bf9c19235563d23e709758fa1244d363
healthbanpangngu-crypto <health.banpangngu@gmail.com>
2026-07-01 09:47:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // ไฟล์ตั้งค่าส่วนกลางสำหรับเชื่อมต่อระบบหน้าบ้าน (Frontend)
-const LIFF_ID = "2010500776-6r1shNwL"; 
-const API_URL = "https://script.google.com/macros/s/AKfycbyzPy7sklwURcJiqFDzlK4OEQ8616CJLuNyAjb8euQNzYJLXz_dj4UJM63w8-nI1L3r/exec";
+const LIFF_ID = "2010563543-9lX9zYFk"; 
+const API_URL = "https://script.google.com/macros/s/AKfycbw6mxBtn77nNB0sBRFea1AThMz0O0VTQYIGvw51A2b6EqRUP_Iyb811wSUZhCk8S2yeJA/exec";
