41f190b6e9953056e203cb75d059fea7577da283
healthbanpangngu-crypto <health.banpangngu@gmail.com>
2026-07-02 17:57:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,13 @@
-// ไฟล์ตั้งค่าส่วนกลางสำหรับเชื่อมต่อระบบหน้าบ้าน (Frontend)
+// การตั้งค่า Firebase สำหรับ รพ.สต.บ้านปางงู
+const firebaseConfig = {
+  apiKey: "AIzaSyBhp4u-c0pRxhkfapr1c0gjgVLTzn4TvUA",
+  authDomain: "health-app-3800a.firebaseapp.com",
+  projectId: "health-app-3800a",
+  storageBucket: "health-app-3800a.firebasestorage.app",
+  messagingSenderId: "992057901349",
+  appId: "1:992057901349:web:59b16d3e66cee51d6b5acc"
+};
+
+// การตั้งค่า LINE LIFF และ Google Apps Script
 const LIFF_ID = "2010563543-9lX9zYFk"; 
 const API_URL = "https://script.google.com/macros/s/AKfycbw6mxBtn77nNB0sBRFea1AThMz0O0VTQYIGvw51A2b6EqRUP_Iyb811wSUZhCk8S2yeJA/exec";
